b3215fbcb94a47c87edd827b98d5defd98856114
old7evenn <stepanukroman9@gmail.com>
2023-07-18 17:25:03 +0300

    docs: create index.htmt, style.css and index.js

---
+++ library/index.js
@@ -0,0 +1,51 @@
+let slideIndex = 1;
+showSlide(slideIndex);
+
+const currentSlide = (n) => showSlide(slideIndex = n)
+
+function showSlide(n) {
+  const slides = document.getElementsByClassName("slider fade")
+  const dots = document.getElementsByClassName("dot")
+
+  if (n > slides.length) slideIndex = 1
+
+  if (n < 1)  slideIndex = slides.length 
+
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "flex";
+  dots[slideIndex - 1].className += " active";
+}
+
+
+const autoSlide = () =>{
+  slideIndex++;
+  showSlide(slideIndex);
+}
+
+setInterval(autoSlide, 3000);
+
+
+const buttons = document.querySelectorAll('.btn');
+
+buttons.forEach(button => {
+  button.addEventListener('click', function() {
+    if (button.classList.contains('btn--active')) {
+      button.classList.remove('btn--active');
+    } else {
+      buttons.forEach(btn => {
+        btn.classList.remove('btn--active');
+        btn.textContent = 'Buy'
+      });
+      button.classList.add('btn--active');
+    }
+    (button.textContent === 'Buy') ? button.textContent = 'Own' : button.textContent = 'Buy';
+  });
+});
+
